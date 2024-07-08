5b8e942c5f468652b09e2822afffe6e08202d726
Md. Abu Bakar Siddiq Mim <abubakarsidiq6871@gmail.com>
2024-07-08 22:59:12 +0600

    frist commit

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,10 @@
+<?php
+// Define the temperature in Celsius
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print the result with 2 decimal points
+printf("The temperature %.2f degrees Celsius is %.2f degrees Fahrenheit.", $celsius, $fahrenheit);
+?>
